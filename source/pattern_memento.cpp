c0cb009a1579e77a0b0645ad75766d568577b88c
kvoistinov <kvoistinov@yandex.ru>
2024-05-16 08:59:42 +0300

    update

--- source/pattern_memento.cpp
+++ source/pattern_memento.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <unordered_map>
 
+#include "gtest/gtest.h"
+
 namespace jc {
 
 class Memento {
@@ -42,9 +44,42 @@ class Caretaker {
   std::unordered_map<std::string, Memento> mementos_;
 };
 
+}  
+
+namespace jc {
+
+ TEST(MementoTest, ValueIsCorrect) {
+   Memento memento(5);
+   ASSERT_EQ(memento.Value(), 5);
+}
+
+TEST(OriginatorTest, SaveAndLoadMemento) {
+   Originator originator(10);
+   Memento memento = originator.ValueMemento();
+   
+   originator.SetValue(20);
+   originator.Load(memento);
+   
+   ASSERT_EQ(originator.ValueMemento().Value(), 10);
+}
+
+TEST(CaretakerTest, SaveAndLoadState) {
+   Caretaker caretaker;
+   Originator originator(30);
+   
+   caretaker.SetState("test", originator.ValueMemento());
+   originator.SetValue(40);
+
+   originator.Load(caretaker.State("test"));
+   
+   ASSERT_EQ(originator.ValueMemento().Value(), 30);
+ }
+
 }  // namespace jc
 
-int main() 
+
+
+int main(int argc, char** argv) 
 {
   std::cout << "TIMP's RK_2\n";
 
@@ -66,4 +101,9 @@ int main()
   originator.PrintValue();  // 1
   originator.Load(caretaker.State("three"));
   originator.PrintValue();  // 3
+
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+
 }
